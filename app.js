6cb5fc9900c6c4746cffcf992ff0b2b963e9bf12
AdrianRC <adrian.rosario.codik@gmail.com>
2017-02-19 17:36:02 -0400

    Add index route.

--- app.js
+++ app.js
@@ -7,22 +7,42 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost/restful_blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 //schema & model config
 var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now()}
+    created: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
-var Blog = new mongoose.model("Blog", blogSchema);
+var Blog = mongoose.model("Blog", blogSchema);
 
 //RESTful routes
-app.get("/blogs", function (req, res) {
 
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+})
+
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {
+                blogs: blogs
+            });
+        }
+    });
 });
+
+
 
 app.get("/blogs/new", function (req, res) {});
 
@@ -36,6 +56,6 @@ app.put("/blogs/:id", function (req, res) {});
 
 app.delete("/blogs/:id", function (req, res) {});
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
